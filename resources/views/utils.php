54b44134c6de5c04f57a4b80bc0c4cb90d7eab16
Yasuyuki Komatsubara <kyasuyako@gmail.com>
2017-07-10 14:52:28 +0900

    fix type and format

--- resources/views/utils.php
+++ resources/views/utils.php
@@ -10,7 +10,7 @@ function calculate_stock($date, $gd, $pt, $sv) {
   $pt_price = floatval($pt);
   $sv_price = floatval($sv);
 
-  $stock['gd_price'] = $gd_price; 
+  $stock['gd_price'] = $gd_price;
   $stock['K24']   = $gd_price * 24   / 24 - 250;
   $stock['K22']   = $gd_price * 22   / 24 - 250;
   $stock['K21.6'] = $gd_price * 21.6 / 24 - 250;
@@ -20,13 +20,13 @@ function calculate_stock($date, $gd, $pt, $sv) {
   $stock['K10']   = $gd_price * 10   / 24 - 500;
   $stock['K9']    = $gd_price *  9   / 24 - 500;
   
-  $stock['pt_price'] = $pt_price; 
+  $stock['pt_price'] = $pt_price;
   $stock['Pt1000'] = $pt_price * 1000 / 1000 - 250;
   $stock['Pt950']  = $pt_price *  950 / 1000 - 250;
   $stock['Pt900']  = $pt_price *  900 / 1000 - 250;
   $stock['Pt850']  = $pt_price *  850 / 1000 - 250;
   
-  $stock['sv_price'] = $sv_price; 
+  $stock['sv_price'] = $sv_price;
   $stock['Sv1000'] = $sv_price * 1000 / 1000 - 20;
   $stock['Sv925']  = $sv_price *  925 / 1000 - 20;
 
@@ -34,6 +34,6 @@ function calculate_stock($date, $gd, $pt, $sv) {
 }
 
 function to_price($num, $suffix = "", $precision = -1) {
-  return number_format( round( $num, $precision ) ) . $suffix;
+  return number_format(round($num, $precision)) . $suffix;
 }
 ?>
